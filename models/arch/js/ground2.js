877ef0ba27c49af94084a4a53f1222883d5776e4
Christian Marques <christian.marques@gmail.com>
2016-02-13 16:14:54 +0000

    primeiro commit com código

---
+++ models/arch/js/ground2.js
@@ -0,0 +1,45 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "ground2.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 4,
+        "faces"         : 1,
+        "normals"       : 1,
+        "colors"        : 0,
+        "uvs"           : 4,
+        "materials"     : 1
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "None",
+	"colorDiffuse" : [0.8, 0.8, 0.8],
+	"colorSpecular" : [0.8, 0.8, 0.8],
+	"illumination" : 2,
+	"opacity" : 0.0,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparent" : true
+	}],
+
+    "vertices": [-2.025555,0.249194,1.445507,1.884708,0.249194,1.445507,1.884708,0.249194,-1.445506,-2.025555,0.249194,-1.445506],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [0,1,0],
+
+    "colors": [],
+
+    "uvs": [[8.6e-05,8.6e-05,0.99991,8.6e-05,0.99991,0.7393,8.6e-05,0.7393]],
+
+    "faces": [43,0,1,2,3,0,0,1,2,3,0,0,0,0]
+
+}
